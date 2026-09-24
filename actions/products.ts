ae1eac8e67092661589389b3f5aa7b23e0201939
paupercloud <andreaprz996@gmail.com>
2026-09-24 15:14:57 -0500

    Fix indentation in products.ts

--- actions/products.ts
+++ actions/products.ts
@@ -140,7 +140,7 @@ export async function saveProduct(input: ProductFormInput) {
         }))
       );
     }
-}
+  }
   revalidatePath("/");
   revalidatePath("/shop");
   revalidatePath(`/product/${slug}`);
